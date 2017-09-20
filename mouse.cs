19047143a5bc72664ff04e635830d7084434dd45
ThreeBGames99 <ThreeBGames99@gmail.com>
2017-09-20 18:27:31 +0200

    proba

---
+++ mouse.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouse : MonoBehaviour {
+
+    public Texture2D myszka;
+    public CursorMode cursor = CursorMode.Auto;
+    public Vector2 v = Vector2.zero;
+
+	// Use this for initialization
+	void Start () {
+        Cursor.SetCursor(myszka, v, cursor);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ ruch.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ruch : MonoBehaviour {
+	public float speed = 10f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 direction = Camera.main.ScreenToWorldPoint (Input.mousePosition) - transform.position;
+		float angle = (Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg) - 90;
+		Quaternion rotation = Quaternion.AngleAxis (angle, Vector3.forward);
+		transform.rotation = Quaternion.Slerp (transform.rotation, rotation, speed * 10);
+	}
+}
